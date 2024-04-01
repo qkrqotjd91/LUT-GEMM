67613117f282dcbe01c6c03a04a3f28b6df9a09b
qkrqotjd91 <qkrqotjd91@gmail.com>
2024-04-01 13:29:31 +0900

    Delete tests directory

--- tests/include/_cublas.h
+++
@@ -1,2 +0,0 @@
-#pragma ones
-void test_cuBlas(int m, int n, int k, bool cmp_check, int iter = 128);
--- tests/include/custom_random.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <time.h>
-
-void random_seed();
-bool rand_bool();
-double rand_fp64(double max=1.0);
-float rand_fp32(float max=1.0);
